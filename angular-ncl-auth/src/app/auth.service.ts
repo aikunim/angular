738d7e88372bbe5812a0a7e8826ca47bff04771d
00048129 <AikunimEs@halykbank.kz>
2020-03-02 17:55:24 +0600

    loading spinner example

--- angular-ncl-auth/src/app/auth.service.ts
+++ angular-ncl-auth/src/app/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,21 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  public async loadingSpinnerAsync(timeout: number){
+    await this.timeout(timeout);
+    console.log(">>>> end async loading >>>>")
+  }
+
+  private timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+  public loadingSpinnerSubscribe(timeout: number): Observable<string> {
+    return new Observable(observer => {
+      setTimeout(() => {
+        observer.next(">>>> end subscribe loading >>>>");
+      }, timeout)
+    })
+  }
 }
--- angular-ncl-auth/src/app/login/login.component.ts
+++ angular-ncl-auth/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loading: boolean = true;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+
+    // Async
+    this.loadAsync();
+
+    // Subscribe
+    // this.loading = true;
+    // this.authService.loadingSpinnerSubscribe(5000).subscribe( message => {
+    //   console.log(message);
+    //   this.loading = false;
+    // });
+  }
+
+  async loadAsync() {
+    this.loading = true;
+    await this.authService.loadingSpinnerAsync(10000);
+    this.loading = false;    
   }
 
 }
